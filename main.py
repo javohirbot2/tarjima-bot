2fdfaed097c9c2ef8dd84b636601d1d2b8356649
Developer Rasulovich <97433981+javohirbot2@users.noreply.github.com>
2022-02-01 18:03:21 +0500

    main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+from telegram.ext import Updater,MessageHandler,CommandHandler,Filters
+from googletrans import Translator
+
+updater = Updater('BOT TOKEN',use_context = True )
+
+# \start komandasi uchun mas'ul funksiya
+@tarjimonbot.message_handler(commands=["start"])
+def salom(message):
+    xabar = "Assalom alaykum, tarjimon botiga xush kelbisiz."
+    xabar += "\nMatningizni yuboring."
+    tarjimonbot.reply_to(message, xabar)
+ 
+def echo(updater,context):
+ usr_msg =updater.message.text
+ translator = Translator()
+ translation = translator.translate(usr_msg,dest='uz') 
+ updater.message.reply_text(translation.text)
+ 
+dp =updater.dispatcher.add_handler
+dp(CommandHandler('start',start))
+dp(MessageHandler(Filters.text,echo))
+
+updater.start_polling()
+updater.idle()
